0bc52947144646239a0342432ddaad7151c20eb4
Max Malinovsky <malinovsky.dev@gmail.com>
2017-02-10 01:30:35 +0200

    Remove debug info

--- app/mailers/gag_mailer.rb
+++ app/mailers/gag_mailer.rb
@@ -7,9 +7,6 @@ class GagMailer < ApplicationMailer
 
     @posts = Post.all.to_a
 
-    p ENV['EMAIL_ADDRESS']
-    p ENV['EMAIL_PASSWORD']
-
     mail(from: ENV['EMAIL_ADDRESS'], to: subscribers, subject: "Fresh top-rated 9gag's gags for you")
 
   end
